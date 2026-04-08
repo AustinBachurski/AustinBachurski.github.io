e091f44dcec5bb656274c6be00192d16f49dcf4f
Austin Bachurski <austinbachurski@gmail.com>
2026-04-08 15:33:14 -0600

    Improve formatting.

--- src/script/commands/helpContent/help.ts
+++ src/script/commands/helpContent/help.ts
@@ -2,35 +2,29 @@ import { BlankLine, HtmlLine, NormalGreenLine } from "../../formatting/terminalC
 import { DivStyle }                             from "../../formatting/styles.js";
 
 export const helpContent = [
-    new NormalGreenLine("JAB bach, version 0.1.12 -It's like bash, but strictly worse!"),
+    new NormalGreenLine("JA bach shell, version 0.1.12 - It's like bash, but strictly worse!"),
     new NormalGreenLine("These shell commands are defined internally.  Type 'help' to see this list."),
     new NormalGreenLine("Type 'help command' to find out more about the function 'command'."),
     new NormalGreenLine("Use 'info bach' to find out more about the shell in general."),
     new BlankLine(),
     new HtmlLine(
         `<span class="${DivStyle.brightGreenText}">  cat</span>`
-            + `<span class="${DivStyle.normalGreenText}"> - Display the contents of a file.</span>`
-    ),
+            + `<span class="${DivStyle.normalGreenText}"> - Display the contents of a file.</span>`),
     new HtmlLine(
         `<span class="${DivStyle.brightGreenText}">clear</span>`
-            + `<span class="${DivStyle.normalGreenText}"> - Clear the terminal display.</span>`
-    ),
+            + `<span class="${DivStyle.normalGreenText}"> - Clear the terminal display.</span>`),
     new HtmlLine(
         `<span class="${DivStyle.brightGreenText}">   cd</span>`
-            + `<span class="${DivStyle.normalGreenText}"> - Change the shell working directory.</span>`
-    ),
+            + `<span class="${DivStyle.normalGreenText}"> - Change the shell working directory.</span>`),
     new HtmlLine(
         `<span class="${DivStyle.brightGreenText}"> exit</span>`
-            + `<span class="${DivStyle.normalGreenText}"> - Close this terminal.</span>`
-    ),
+            + `<span class="${DivStyle.normalGreenText}"> - Close this terminal.</span>`),
     new HtmlLine(
         `<span class="${DivStyle.brightGreenText}"> help</span>`
-            + `<span class="${DivStyle.normalGreenText}"> - Display information about how to use a command. ex. 'help help'</span>`
-    ),
+            + `<span class="${DivStyle.normalGreenText}"> - Display information about how to use a command. ex. 'help help'</span>`),
     new HtmlLine(
         `<span class="${DivStyle.brightGreenText}">   ls</span>`
-            + `<span class="${DivStyle.normalGreenText}"> - List the contents of the current directory.</span>`
-    ),
+            + `<span class="${DivStyle.normalGreenText}"> - List the contents of the current directory.</span>`),
 ] ;
 
 export const helpHelpContent = [
